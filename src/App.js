5fc2506d8741a60eefe80ca4fabed30c8f495683
Nitya Dosaj <nitya.dosaj@gmail.com>
2021-11-25 13:22:36 -0800

    implementation completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import React from 'react';
-import RecipeList from './components/RecipeList';
+import React, {useState} from 'react';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
-import { render } from '@testing-library/react';
+import Recipe from './components/Recipe';
+import Alert from './components/Alert';
 
 function App() {
-  render()
-  {
-    return (
-      <RecipeList/>
-    );
-  }
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
+
+  const url =`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=053f66cd&app_key=977184b019927b0735bdc41312d1881a`;
   
+  const getRecipe = async () =>{
+    if(query !== "")
+    {
+      const res = await axios.get(url);
+      if(res.data.more){
+        return setAlert("No food with such name");
+      }
+      setRecipes(res.data.hits);
+      console.log(res);
+      setAlert("");
+      setQuery("");
+    }
+    else{
+      setAlert("Please fill the form");
+    }
+  }
+
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    getRecipe();
+  }
+
+  const onChange = (e)=>{
+   setQuery(e.target.value);
+  }
+  return(
+    <div className='App'>
+      <h1>Food Searching App</h1>
+      <form className="search-form" onSubmit={onSubmit}>
+      {alert !== "" && <Alert alert={alert}/>}
+        <input type="text" placeholder="Search Food" autoComplete="off" onChange={onChange} value={query}/>
+        <input type="submit" value="Search"/>
+      </form>
+      <div className="recipes">
+        {recipes !== [] && recipes.map(recipe =>
+          <Recipe key={uuidv4()} recipe={recipe}/>)}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/URL.js
+++
@@ -1,2 +0,0 @@
-const YOUR_URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=2db02c4a283c431994c7c1e33a988fb7&includeNutrition=true`;
-export default YOUR_URL;
